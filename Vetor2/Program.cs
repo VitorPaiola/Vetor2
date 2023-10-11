5244dee80613195641f7e0ac7909480ac37feffa
Vitor Paiola <vitinho.souza1234@gmail.com>
2023-10-11 16:22:11 -0300

    Adicionar arquivos de projeto.

---
+++ Vetor2/Program.cs
@@ -0,0 +1,25 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+int[] nums = new int[n];
+
+string[] vet = Console.ReadLine().Split(' ');
+
+for (int i = 0; i < n; i++) {
+    nums[i] = int.Parse(vet[i]);
+}
+
+
+for (int i = 0; i < n; i++) {
+    if (nums[i] % 2 == 0) {
+        Console.Write(nums[i] + " ");
+    }
+}
+
+int cont = 0;
+for (int i = 0; i < n; i++) {
+    if (nums[i] % 2 == 0) {
+        cont++;
+    }
+}
+
+Console.WriteLine("\n" + cont);
